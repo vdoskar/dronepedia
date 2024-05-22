a83acb4234304718f6e7a663a2c4056ea4b123f9
vdoskar <vladadoskar@gmail.com>
2024-05-22 23:32:58 +0200

    sql empty database

---
+++ src/sql/dronepedia_empty.sql
@@ -0,0 +1,269 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Stř 22. kvě 2024, 23:29
+-- Verze serveru: 10.4.32-MariaDB
+-- Verze PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databáze: `dronepedia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `slug` varchar(64) NOT NULL,
+  `title` varchar(64) NOT NULL,
+  `short_summary` varchar(150) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `category` varchar(64) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  `status` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Vypisuji data pro tabulku `posts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `posts_attachments`
+--
+
+CREATE TABLE `posts_attachments` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `type` varchar(32) NOT NULL,
+  `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Vypisuji data pro tabulku `posts_attachments`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `posts_comments`
+--
+
+CREATE TABLE `posts_comments` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `date_created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Vypisuji data pro tabulku `posts_comments`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `uuid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `date_registered` datetime NOT NULL,
+  `email` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `username` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `label` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `role` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  `date_last_login` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='Table that stores all registered users';
+
+--
+-- Vypisuji data pro tabulku `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `users_drones`
+--
+
+CREATE TABLE `users_drones` (
+  `id` int(11) NOT NULL,
+  `owner` varchar(255) DEFAULT NULL,
+  `drone_name` varchar(255) DEFAULT NULL,
+  `drone_description` varchar(255) DEFAULT NULL,
+  `drone_params` text DEFAULT NULL COMMENT 'json of raw params',
+  `drone_img` varchar(255) DEFAULT NULL COMMENT 'url to the image'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Vypisuji data pro tabulku `users_drones`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `users_logged`
+--
+
+CREATE TABLE `users_logged` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `logged_since` datetime NOT NULL,
+  `logged_until` datetime DEFAULT NULL,
+  `session_token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Vypisuji data pro tabulku `users_logged`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `users_settings`
+--
+
+CREATE TABLE `users_settings` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `bio` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pic_profile` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pic_banner` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Vypisuji data pro tabulku `users_settings`
+--
+
+--
+-- Indexy pro exportované tabulky
+--
+
+--
+-- Indexy pro tabulku `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `post_slug` (`slug`);
+
+--
+-- Indexy pro tabulku `posts_attachments`
+--
+ALTER TABLE `posts_attachments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_id` (`post_id`);
+
+--
+-- Indexy pro tabulku `posts_comments`
+--
+ALTER TABLE `posts_comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexy pro tabulku `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_uuid` (`uuid`);
+
+--
+-- Indexy pro tabulku `users_drones`
+--
+ALTER TABLE `users_drones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexy pro tabulku `users_logged`
+--
+ALTER TABLE `users_logged`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `logged_user` (`user`);
+
+--
+-- Indexy pro tabulku `users_settings`
+--
+ALTER TABLE `users_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pro tabulky
+--
+
+--
+-- AUTO_INCREMENT pro tabulku `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT pro tabulku `posts_attachments`
+--
+ALTER TABLE `posts_attachments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT pro tabulku `posts_comments`
+--
+ALTER TABLE `posts_comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT pro tabulku `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pro tabulku `users_drones`
+--
+ALTER TABLE `users_drones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pro tabulku `users_logged`
+--
+ALTER TABLE `users_logged`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT pro tabulku `users_settings`
+--
+ALTER TABLE `users_settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Omezení pro exportované tabulky
+--
+
+--
+-- Omezení pro tabulku `users_logged`
+--
+ALTER TABLE `users_logged`
+  ADD CONSTRAINT `user_logged` FOREIGN KEY (`user`) REFERENCES `users` (`uuid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
